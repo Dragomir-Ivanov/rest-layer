6d9a80b8cb0c328563bfc392b8a7f9059c1ba11c
Dragomir Ivanov <drago.ivanov@gmail.com>
2023-12-01 18:00:22 +0200

    schema.Null: support pointer types

--- schema/null.go
+++ schema/null.go
@@ -1,14 +1,18 @@
 package schema
 
-import "errors"
+import (
+	"errors"
+	"reflect"
+)
 
 // Null validates that the value is null.
 type Null []FieldValidator
 
 // Validate ensures that value is null.
 func (v Null) Validate(value interface{}) (interface{}, error) {
-	if value != nil {
-		return nil, errors.New("not null")
+	if value == nil || (reflect.TypeOf(value).Kind() == reflect.Ptr && reflect.ValueOf(value).IsNil()) {
+		return value, nil
 	}
-	return value, nil
+
+	return value, errors.New("not null")
 }
